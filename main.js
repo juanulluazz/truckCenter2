c50b099d1bf5ddb0a2c4c77955abe681136a077f
juanulluazz <52545002+juanulluazz@users.noreply.github.com>
2022-01-10 23:16:31 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+$(window).scroll(function(event) {
+    var scrollLeft = $(window).scrollLeft();
+    let scrollTop = $(window).scrollTop();
+   
+    if(scrollTop >= 50){
+    //   document.getElementById('test').style.opacity='0.5';
+
+      document.getElementById('nav-scroll').style.backgroundColor='rgba(124, 42, 21, 0.9)';
+    }
+  
+   if(scrollTop <= 50){
+      document.getElementById('nav-scroll').style.backgroundColor='#7c2a15'
+
+    }
+  
+  });
+
+
+
+const ham = document.querySelector(".ham");
+const enlaces = document.querySelector('.enlaces-menu');
+const barras = document.querySelectorAll('.ham span');
+
+ham.addEventListener('click', () => {
+    enlaces.classList.toggle('activado');
+    barras.forEach(child => {child.classList.toggle('animado')});
+});
