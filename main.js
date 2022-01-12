ed4656d28f658a0d153bc6f31468abba104aa0a7
juanulluazz <52545002+juanulluazz@users.noreply.github.com>
2022-01-12 02:21:07 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ $(window).scroll(function(event) {
     var scrollLeft = $(window).scrollLeft();
     let scrollTop = $(window).scrollTop();
    
-    if(scrollTop >= 50){
+    if(scrollTop >= 50 ){
     //   document.getElementById('test').style.opacity='0.5';
 
       document.getElementById('nav-scroll').style.backgroundColor='rgba(124, 42, 21, 0.9)';
     }
   
-   if(scrollTop <= 50){
+   if(scrollTop <= 50 ){
       document.getElementById('nav-scroll').style.backgroundColor='#7c2a15'
 
     }
